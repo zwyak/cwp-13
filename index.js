e80d3d19187cd3d62928c59aa2e27fadcb61f7ce
zwyak <vlad.senyuk97@gmail.com>
2020-02-20 17:36:57 +0300

    Added express

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const Sequelize = require('sequelize');
 const config = require('./config.json');
 const db = require('./models')(Sequelize, config);
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const fleetsRouter = express.Router();
+const motionsRouter = express.Router();
+const vehiclesRouter = express.Router();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Server app listening on port 3000!');
+})
