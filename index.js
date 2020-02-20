a96a3d31ead5ccdb341f8630c01dbef785574384
zwyak <vlad.senyuk97@gmail.com>
2020-02-20 17:56:42 +0300

    Added read fleets by id

--- index.js
+++ index.js
@@ -17,7 +17,24 @@ fleetsRouter.get('/readall', (req, res) => {
   });
 });
 
+fleetsRouter.get('/read', (req, res) => {
+  db.fleets.findByPk(req.query.id, {raw: true}).then((f) =>{
+    res.send(f);
+  })
+  .catch((err) =>{
+    res.sendStatus(404);
+  });
+});
+
 app.use("/api/fleets", fleetsRouter);
+
+vehiclesRouter.get('/readall', (req, res) => {
+  db.vehicles.findAll({raw: true}).then((f) =>{
+    res.send(f);
+  });
+});
+
+app.use("/api/fleets", vehiclesRouter);
 
 app.listen(3000, () => {
   console.log('Server app listening on port 3000!');
